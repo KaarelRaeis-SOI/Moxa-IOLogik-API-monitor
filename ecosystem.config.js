623061e1d8990353e069890d87787ca7662bb295
Kaarel.R <krais@schmidtocean.org>
2024-05-29 12:14:19 +0300

    Break logs into days

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+      {
+        name: 'moxa-api',
+        script: 'app.py',
+        interpreter: '~/miniconda3/envs/moxa-api/bin/python',  // Update this path
+        watch: false,
+        env: {
+          PYTHONUNBUFFERED: "1"
+        }
+      }
+    ]
+  };
+  
